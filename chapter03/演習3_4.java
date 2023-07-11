eb0b154d59e4879a644beedd9e9ad76381875178
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2023-07-11 17:24:13 +0900

    いつか

--- chapter03/演習3_4.java
+++ chapter03/演習3_4.java
@@ -11,10 +11,7 @@ public class 演習3_4 {
             System.out.printf("%dは存在しません。%n", target);
         }
     }
-    public static int binarySearch(int[] numbers, int target) {
-        int left = 0;
-        int right = numbers.length - 1;
-        ////////////////(debug start)//////////////////
+    public static void printHeader(int[] numbers) {
         System.out.print("   |");
         for (int i = 0; i < numbers.length; i++) {
             System.out.printf("  %2d ", i);
@@ -25,26 +22,36 @@ public class 演習3_4 {
             System.out.printf("-----", i);
         }
         System.out.println();
+        
+    }
+    public static void printBody(int[] numbers, int left, int mid, int right) {
+        System.out.printf("   |");
+        for (int i = 0; i < numbers.length; i++) {
+            if (i == left && i == mid && i == right) System.out.printf("<-+->");
+            else if (i == left && i == mid) System.out.printf("<-+  ");
+            else if (i == mid && i == right) System.out.printf("  +->");
+            else if (i == left) System.out.printf("<-   ");
+            else if (i == mid)  System.out.printf("   + ");
+            else if (i == right) System.out.printf("   ->");
+            else System.out.print("     ");
+        }
+        System.out.println();
+        System.out.printf("%3d|", mid);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.printf("  %2d ", numbers[i]);
+        }
+        System.out.println();
+    }
+    public static int binarySearch(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        ////////////////(debug start)//////////////////
+        printHeader(numbers);
         ////////////////(debug end)////////////////////
         while (left <= right) {
             int mid = (left + right) / 2;
             ////////////////(debug start)//////////////////
-            System.out.printf("   |");
-            for (int i = 0; i < numbers.length; i++) {
-                if (i == left && i == mid && i == right) System.out.printf("<-+->");
-                else if (i == left && i == mid) System.out.printf("<-+  ");
-                else if (i == mid && i == right) System.out.printf("  +->");
-                else if (i == left) System.out.printf("<-   ");
-                else if (i == mid)  System.out.printf("   + ");
-                else if (i == right) System.out.printf("   ->");
-                else System.out.print("     ");
-            }
-            System.out.println();
-            System.out.printf("%3d|", mid);
-            for (int i = 0; i < numbers.length; i++) {
-                System.out.printf("  %2d ", numbers[i]);
-            }
-            System.out.println();
+            printBody(numbers, left, mid, right);
             ////////////////(debug end)////////////////////
             if (target > numbers[mid]) {
                 left = mid + 1;
