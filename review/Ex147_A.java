59db47b76ac15cefe54c819f77c6649f7b3589ee
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2023-09-13 00:08:36 +0900

    2023年09月13日

---
+++ review/Ex147_A.java
@@ -0,0 +1,24 @@
+package review;
+
+import java.util.Scanner;
+
+public class Ex147_A {
+
+	//https://atcoder.jp/contests/abc147/tasks/abc147_a
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner (System.in);
+		System.out.println("3つの整数を入力せよ");
+		int num01 = sc.nextInt();
+		int num02 = sc.nextInt();
+		int num03 = sc.nextInt();
+		
+		if(num01 + num02 + num03 >= 22) {
+			System.out.println("bust");
+		}else {
+			System.out.println("win");
+		}
+		
+	}
+
+}
---
+++ review/Ex147_B.java
@@ -0,0 +1,27 @@
+package review;
+
+import java.util.Scanner;
+
+public class Ex147_B {
+
+	//https://atcoder.jp/contests/abc147/tasks/abc147_b
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		
+		char[] chararray = str.toCharArray();
+		int count = 0;
+		
+		int half = chararray.length/2;
+		
+		for(int i = 0; i < half; i++) {
+			if(chararray[i] != chararray[chararray.length -i-1]) {
+				count++;
+			}
+		}
+		System.out.println(count);
+		
+	}
+
+}
