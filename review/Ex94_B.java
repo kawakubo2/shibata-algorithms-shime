677b30e96459abf56b149a6a38718dde24549cfc
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2024-03-10 02:15:04 +0900

    2024年03月10日分

---
+++ review/Ex94_B.java
@@ -0,0 +1,33 @@
+package review;
+
+import java.util.Scanner;
+
+public class Ex94_B {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int x = sc.nextInt();
+		
+		int[] a = new int[n + 1];
+		
+		int L = 0;
+		int R = 0;
+		
+		for(int i = 0; i < m; i++) {
+			a[i] = sc.nextInt();
+			if(a[i] < x) {
+				L ++;
+			}else {
+				R++;
+		}
+		}
+		System.out.println(Math.min(R, L));
+		
+		sc.close();
+
+	}
+
+}
---
+++ review_kawakubo/Ex94_B_川久保.java
@@ -0,0 +1,76 @@
+package review_kawakubo;
+
+import java.util.Scanner;
+
+public class Ex94_B_川久保 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int x = sc.nextInt();
+		
+		/*
+		 * 川久保注
+		 * ここはマス目の数ではなく料金所のかずなので
+		 * n + 1の要素数ではなくmの要素数分作る
+		 * 料金所の数がマス目の数を超えてしまっている。
+		 */
+		// int[] a = new int[n + 1];
+		int[] a = new int[m];
+		
+		for(int i = 0; i < m; i++) {
+			a[i] = sc.nextInt();
+		}
+
+		System.out.println(calcMinPath(m, a, x));
+		
+		sc.close();
+	}
+
+	public static int calcMinPath(int m, int[] a, int x) {
+		/*
+		 * 川久保注
+		 * 2分探索するが決してxは見つかることはない。
+		 * なぜなら、xは料金所ではないという制約があるから。
+		 * つまり見つかったindexにある値はxより大きいこと
+		 * 小さいこともありうる。
+		 * その調整をしているのが
+		 * 
+		 * int leftCount = x > a[searchIndex] ? searchIndex + 1: searchIndex;
+		 * 
+		 * indexは0始まりなので、xが見つかったindexの値よりも大きい場合はindex+1、
+		 * xが見つかったindexの値より小さい場合、indexが左側にある料金所の
+		 * 数となる。
+		 * 
+		 * 料金所の数はm - leftCountなので、最小値がleftCountとm - leftCount
+		 * の最小値が一番コストがかからないことになる。
+		 * 
+		 * return Math.min(leftCount, m - leftCount);
+		 * 
+		 */
+		int searchIndex = binarySearch(a, x);
+		int leftCount = x > a[searchIndex] ? searchIndex + 1: searchIndex;
+
+		return Math.min(leftCount, m - leftCount);
+	}
+
+	public static int binarySearch(int[] a, int x) {
+		int left  = 0;
+		int right = a.length - 1;
+		int mid = -1;
+		while (left <= right) {
+			mid = (left + right) / 2;
+			if (x > a[mid]) {
+				left = mid + 1;
+			} else if (x < a[mid]) {
+				right = mid - 1;
+			} else {
+				throw new IllegalStateException("いる場所が料金所");
+			}
+		}
+		return mid;
+	}
+
+}
