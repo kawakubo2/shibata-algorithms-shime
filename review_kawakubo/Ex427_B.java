c47cee99471a0906acf4356bb78b05a41fc131a3
kawakubo <tomo.kawakubo@gmail.com>
2025-10-22 00:07:19 +0900

    2025年10月21日分

---
+++ review_kawakubo/Ex427_B.java
@@ -0,0 +1,30 @@
+package review_kawakubo;
+
+import java.util.Scanner;
+
+public class Ex427_B {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 1;
+		for (int i = 1; i < n; i++) {
+			System.out.println("sum= " + sum);
+			sum += digit(sum);
+		}
+		System.out.println(sum);
+		sc.close();
+	}
+
+	private static int digit(int n) {
+		int sum = 0;
+		while (n != 0) {
+			System.out.println("n= " + n);
+			System.out.println("n%10= " + n % 10);
+			sum += (n % 10);
+			n /= 10;
+		}
+		return sum;
+	}
+
+}
